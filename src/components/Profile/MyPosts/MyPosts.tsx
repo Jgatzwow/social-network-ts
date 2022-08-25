6b340ce601d8a232ca2cb025a0c55c7c3192cb1d
MSokol <mihasokol1993@gmail.com>
2022-08-25 18:34:13 +0300

    Made MyPosts input element controlled and created a function that adds posts, also refactored Post component to be mapped

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -1,21 +1,49 @@
-import React from "react";
-import {Post} from "./Post/Post";
-import styles from "./MyPosts.module.css"
+import React, {ChangeEvent, useState} from 'react';
+import {Post} from './Post/Post';
+import styles from './MyPosts.module.css'
+
+type InitialStateType = Array<MessagesType>
+type MessagesType = {
+    id: number
+    post: string
+    likes: number
+}
+
+const initialState = [
+    {id: 1, post: 'hello', likes: 123},
+    {id: 2, post: 'sup', likes: 14123},
+    {id: 3, post: 'Bye', likes: 23},
+    {id: 4, post: 'Aloha', likes: 13},
+]
 
 
 export const MyPosts = () => {
+
+    const [messages, setMessages] = useState<InitialStateType>(initialState)
+    const [onPostChange, setOnPostChange] = useState('')
+    const onAddMessage = (value: string) => {
+        setMessages([{id: 5, post: value, likes: 0}, ...messages])
+    }
+    const onAddMessageHandler = () => {
+        onAddMessage(onPostChange)
+        setOnPostChange('')
+    }
+    const onInputChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setOnPostChange(e.currentTarget.value)
+    }
+
     return (
         <div className={styles.my__post__wrapper}>
             <h3>My posts</h3>
             <div>
-                <textarea value={"your post"}></textarea>
-                <button className={styles.button}>send</button>
+                <input onChange={onInputChangeHandler} value={onPostChange}></input>
+                <button onClick={onAddMessageHandler} className={styles.button}>add post
+                </button>
             </div>
-            <Post likes={243} message={"hello"}/>
-            <Post likes={22453} message={"bye"}/>
-            <Post likes={25543} message={"test"}/>
-            <Post likes={255643} message={"Yo"}/>
-            <Post likes={24631323} message={"MessagePost"}/>
+            {messages.map(m => {
+                return <Post key={m.id} likes={m.likes} message={m.post}/>
+            })}
+
         </div>
     )
 }
--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import {MyPosts} from './MyPosts/MyPosts';
-import styles from './Profile.module.css'
 import {ProfileInfo} from './ProfileInfo/ProfileInfo';
 
 
